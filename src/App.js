40e9bc639a251049dc302dd797b0219722d5627c
YIMIN LI <yimin.li@student.kuleuven.be>
2022-12-06 22:22:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { legacy_createStore as createStore, combineReducers, applyMiddleware } from "redux";
 import { taskMiddleware } from "react-palm/tasks";
 import { Provider } from "react-redux";
 import KeplerGl from "kepler.gl";
 import keplerGlReducer from "kepler.gl/reducers";
-// import { addDataToMap } from "kepler.gl/actions";
-// import useSWR from "swr";, useDispatch
+// import {token} from "./mapbox";
 
 
 const reducers = combineReducers({
@@ -14,53 +13,47 @@ const reducers = combineReducers({
 
 const store = createStore(reducers, {}, applyMiddleware(taskMiddleware));
 
+function writeCookie(){
+    if(document.cookie === "")
+    {
+        document.cookie = prompt('Please enter your mapbox access mapboxAccessToken here:');
+    }
+    return document.cookie;
+}
+const token = writeCookie();
+
 function App() {
-  return (
-    <Provider store={store}>
-      <Map />
-    </Provider>
-  );
+    // var [mapboxAccessToken, setToken] = useState("");
+    //
+    // useEffect(()=>{
+    //     getToken();
+    //     console.log(mapboxAccessToken);
+    // },[mapboxAccessToken]);
+    //
+    // async function getToken() {
+    //     try{
+    //         const res = await fetch(raw)
+    //         const data = await res.text();
+    //         setToken(data);
+    //     } catch (err){
+    //         console.error(err);
+    //     }
+    writeCookie();
+      return (
+          <div className="App">
+                <Provider store={store}>
+                    <Map/>
+                </Provider>
+          </div>
+      );
 }
 
 function Map() {
-  
-  // const dispatch = useDispatch();
-  // const fetcher = (url) => fetch(url).then((res) => res.json());
-  // const { data } = useSWR(
-  //   "https://gist.githubusercontent.com/FlorentLee/25f29315b1973b09adac3c062f521858/raw/22d970452089192f6ca3cb637387b6b58f522bb9/data.json",
-  //   fetcher);
-  //
-  // React.useEffect(() => {
-  //   if (data) {
-  //     dispatch(
-  //       addDataToMap({
-  //         datasets: {
-  //           info: {
-  //             label: "Omicron Cases in Belgium",
-  //             id: "omicron_belgium"
-  //           },
-  //           data
-  //         },
-  //         option: {
-  //           centerMap: true,
-  //           readOnly: false
-  //         },
-  //         config: {
-  //           mapStyle: {styleType: 'dark'}
-  //         }
-  //       })
-  //     );
-  //   }
-  // }, [dispatch, data]);
-
-  return (
-    <KeplerGl
-      id="SpreadGL"
-      mapboxApiAccessToken = "pk.eyJ1IjoiZGFqYXAiLCJhIjoiY2t6bzhyYzY5NGxuYTJubzF2anNzNjBxdiJ9.OQ8pxr8aRxtFSmlPg8NOvg"
-      width={window.innerWidth}
-      height={window.innerHeight}
-    />
-  );
+    return <KeplerGl
+        id="SpreadGL"
+        mapboxApiAccessToken = {token}
+        width={window.innerWidth}
+        height={window.innerHeight}/>
 }
 
 export default App;
