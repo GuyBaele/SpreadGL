954b60c2beb6f1b91faf9c68f5b2de60746fd8c7
YIMIN LI <yimin.li@student.kuleuven.be>
2022-12-14 00:42:50 +0100

    Update main.py

--- TreeProcessingTool/main.py
+++ TreeProcessingTool/main.py
@@ -12,8 +12,7 @@ def main():
                         help='At the end of sequence names, find the date format. '
                              'If it is a decimal number, enter "decimal". '
                              'If it is ISO-8601 (Year-Month-Day), enter "yyyy-mm-dd". '
-                             'When it is incomplete, the first day of the corresponding month or year will be applied.'
-)
+                             'When it is incomplete, the first day of the corresponding month or year will be applied.')
     parser.add_argument('--location', '-l', required=True,
                         help='Enter the two annotations, storing latitudes and longitudes in this order, with a comma separator. '
                              'If there is only one annotation that stores either coordinates or location names, enter this annotation without comma.')
