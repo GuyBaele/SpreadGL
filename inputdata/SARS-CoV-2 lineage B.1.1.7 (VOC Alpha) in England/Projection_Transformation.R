f8e3e2918fe0051a368b4650161e773f02fdefe9
YIMIN LI <yimin.li@student.kuleuven.be>
2022-12-16 16:33:53 +0100

    Update Projection_Transformation.R

--- inputdata/SARS-CoV-2 lineage B.1.1.7 (VOC Alpha) in England/Projection_Transformation.R
+++ inputdata/SARS-CoV-2 lineage B.1.1.7 (VOC Alpha) in England/Projection_Transformation.R
@@ -1,9 +1,9 @@
 library(sp)
-input <- read.csv("/InputPath/Output_UK_Projection_1st.csv", header=TRUE, stringsAsFactors=FALSE)
-coords_uk_start = data.frame(lon=input[, c("start_longitude")], lat=input[, c("start_latitude")])
-coords_uk_end = data.frame(lon=input[, c("end_longitude")], lat=input[, c("end_latitude")])
-coordinates(coords_uk_start) = c("lon", "lat")
-coordinates(coords_uk_end) = c("lon", "lat")
+input <- read.csv("Output_UK_Projection_1st.csv", header=TRUE, stringsAsFactors=FALSE)
+coords_uk_start = data.frame(start_lon=input[, c("start_longitude")], start_lat=input[, c("start_latitude")])
+coords_uk_end = data.frame(end_lon=input[, c("end_longitude")], end_lat=input[, c("end_latitude")])
+coordinates(coords_uk_start) = c("start_lon", "start_lat")
+coordinates(coords_uk_end) = c("end_lon", "end_lat")
 uk_projection = CRS("+init=epsg:27700")
 proj4string(coords_uk_start) = uk_projection
 proj4string(coords_uk_end) = uk_projection
@@ -14,4 +14,4 @@ coords_wgs84_start = coords_wgs84_start@coords
 coords_wgs84_end = coords_wgs84_end@coords
 coords_wgs84_start_end <-cbind(coords_wgs84_start, coords_wgs84_end)
 output <-cbind(input, coords_wgs84_start_end)
-write.csv(output,"/OutputPath/Output_WGS84_RConverted_2nd.csv", row.names = FALSE)
+write.csv(output,"Output_Converted_2nd.csv", row.names = FALSE)
