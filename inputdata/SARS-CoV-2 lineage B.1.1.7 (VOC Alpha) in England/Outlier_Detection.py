2bcbf2bb031942bc77c62bff74516f4ef3d575ed
YIMIN LI <yimin.li@student.kuleuven.be>
2022-12-16 16:33:30 +0100

    Update Outlier_Detection.py

--- inputdata/SARS-CoV-2 lineage B.1.1.7 (VOC Alpha) in England/Outlier_Detection.py
+++ inputdata/SARS-CoV-2 lineage B.1.1.7 (VOC Alpha) in England/Outlier_Detection.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
-file1 = open("/InputPath/Output_Combined_3rd.csv")
+file1 = open("/Users/u0150975/Downloads/Visualisation/Continuous Space Examples/B.1.1.7_England/Output_Converted_2nd.csv")
 df1 = pd.read_csv(file1, delimiter=",")
 arr1 = []
 arr2 = []
 for i in range(len(df1)):
-    arr1.append(df1.loc[i, "uk_start_latitude"])
-    arr2.append(df1.loc[i, "uk_end_latitude"])
+    arr1.append(df1.loc[i, "start_latitude"])
+    arr2.append(df1.loc[i, "end_latitude"])
 print(len(arr1))
 
-file2 = open("/InputPath/metadata_for_check.csv")
+file2 = open("/Users/u0150975/Downloads/Visualisation/Continuous Space Examples/B.1.1.7_England/metadata_for_check.csv")
 df2 = pd.read_csv(file2, delimiter=",")
 arr3 = []
 arr4 = []
@@ -24,8 +24,8 @@ for i in range(len(arr1)):
     if arr1[i] in arr3 or arr2[i] in arr4:
         df1.drop(i, inplace=True)
         counter += 1
-print(counter) 
 print(df1)
-
-df1 = df1.drop(columns=['uk_start_latitude', 'uk_start_longitude', 'uk_end_latitude', 'uk_end_longitude'])
-df1.to_csv("/OutputPath/Final_Output_4th.csv", sep=",", index=False)
+print(counter)
+df1 = df1.drop(columns=['start_latitude', 'start_longitude', 'end_latitude', 'end_longitude'])
+print(df1)
+df1.to_csv("/Users/u0150975/Downloads/Visualisation/Continuous Space Examples/B.1.1.7_England/Output_WGS_84_3rd.csv", sep=",", index=False)
