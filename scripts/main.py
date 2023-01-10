a39be88b649535f9462e5bd1958200c5bda2ee47
YIMIN LI <yimin.li@student.kuleuven.be>
2023-01-11 00:54:22 +0100

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -9,17 +9,17 @@ def main():
     parser.add_argument('--tree', '-tr', required=True,
                         help='Specify the file name of your MCC tree with filename extension.')
     parser.add_argument('--date', '-d', choices=['float', 'datetime'], required=True,
-                        help='At the end of sequence names, find the format of date information. '
+                        help='At the end of sequence names, you can typically find the date format. '
                              'If it is a floating-point number, enter "float". '
                              'If it is ISO-8601 (Year-Month-Day/yyyy-mm-dd), enter "datetime". '
-                             'When it is incomplete, the first day of the corresponding month or year will be applied.')
+                             'When it is incomplete, the first day of the corresponding month or year will be used.')
     parser.add_argument('--location', '-l', required=True,
-                        help='Enter the two annotations, storing latitudes and longitudes in this order, with a comma separator. '
-                             'If there is only one annotation that stores either coordinates or location names, enter this annotation without comma.')
+                        help='Enter the two annotations, storing latitudes and longitudes (in this order), with a comma separator. '
+                             'If there is only one annotation that stores either coordinates or location names, enter this annotation without a comma.')
     parser.add_argument('--list', '-li',
-                        help='Optional, only mandatory for discrete space: Specify the file name of your list of coordinates with filename extension. '
-                             'This file should be in the format of ".csv" with the separator of "," '
-                             'and comprised of three columns with a specific header of "location,latitude,longitude".')
+                        help='Optional, only mandatory for discrete space. Specify the file name of your list of coordinates with filename extension. '
+                             'This file should be in the csv format with a comma (",") separator, '
+                             'and should be comprised of three columns with a specific header of "location,latitude,longitude".')
     parser.add_argument('--type', '-t',  choices=['csv'],
                         help='Optional: Type in "csv" if you would like to inspect your output file in a tabular format.')
 
