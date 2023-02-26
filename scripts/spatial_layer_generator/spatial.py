7292f54c5e8e8200043a1340a289f7767282c214
YIMIN LI <yimin.li@student.kuleuven.be>
2023-02-26 23:53:01 +0100

    Update spatial.py

--- scripts/spatial_layer_generator/spatial.py
+++ scripts/spatial_layer_generator/spatial.py
@@ -6,29 +6,32 @@ from spatial_layer_generator.discrete_space_processor import *
 def main():
     welcome = "Welcome to the spatial layer generator! You can create a spatial layer for a phylogenetic tree to display in Spread.gl."
     parser = argparse.ArgumentParser(description=welcome)
-    parser.add_argument('--tree', '-t', required=True,
+    parser.add_argument('--tree', '-tr', required=True,
                         help='Specify the file name of your phylogenetic tree with filename extension.')
-    parser.add_argument('--location', '-l', required=True,
+    parser.add_argument('--time', '-ti', required=True,
+                        help='Specify the date of the most recent tip in the format of either decimal year or YYYY-MM-DD.')
+    parser.add_argument('--location', '-lo', required=True,
                         help='Type in the annotation that stores the location information or coordinates. '
                              'Or type in the two annotations storing latitude and longitude (in this order) with a comma separator.')
     parser.add_argument('--list', '-li',
-                        help='Optional, only mandatory in the case of discrete space. '
+                        help='Only needed in the case of discrete space. '
                              'Specify the file name of your list of coordinates with filename extension. '
                              'This file should be in the csv format with a comma (",") separator, '
-                             'and should be comprised of three columns with a specific header of "location,latitude,longitude".')
+                             'and comprised of three columns with a specific header of "location,latitude,longitude".')
     parser.add_argument('--format', '-f',  choices=['csv'],
-                        help='Optional: Type in "csv" if you would like to inspect your output file in a tabular format.')
+                        help='Optional: If you want to check the output in a table, use "csv" in this argument.')
 
     args = parser.parse_args()
     tree = str(args.tree)
+    mostRecentTip = str(args.time)
     location = str(args.location).split(',')
     location_list = str(args.list)
     extension = str(args.format)
 
     if location_list == 'None':
-        process_continuous_phylogeography(tree, location, extension)
+        process_continuous_phylogeography(tree, mostRecentTip, location, extension)
     else:
-        process_discrete_phylogeography(tree, location, location_list, extension)
+        process_discrete_phylogeography(tree, mostRecentTip, location, location_list, extension)
 
 
 # Run with command line arguments precisely when called directly
