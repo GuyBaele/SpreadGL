5274bc4e0567465057426459afb2e7319de0fb33
YIMIN LI <yimin.li@student.kuleuven.be>
2023-02-27 20:40:33 +0100

    Update and rename spatial.py to space.py

--- scripts/spatial_layer_generator/space.py
+++ scripts/spatial_layer_generator/space.py
@@ -7,19 +7,19 @@ def main():
     welcome = "Welcome to the spatial layer generator! You can create a spatial layer for a phylogenetic tree to display in Spread.gl."
     parser = argparse.ArgumentParser(description=welcome)
     parser.add_argument('--tree', '-tr', required=True,
-                        help='Specify the file name of your phylogenetic tree with filename extension.')
+                        help='Specify the name of your input tree file with filename extension.')
     parser.add_argument('--time', '-ti', required=True,
-                        help='Specify the date of the most recent tip in the format of either decimal year or YYYY-MM-DD.')
+                        help='Enter the date of the most recent tip. It can be either in the format of YYYY-MM-DD or decimal year.')
     parser.add_argument('--location', '-lo', required=True,
-                        help='Type in the annotation that stores the location information or coordinates. '
-                             'Or type in the two annotations storing latitude and longitude (in this order) with a comma separator.')
+                        help='Type in the annotation that stores the location information (names or coordinates). '
+                             'If there are two annotations to store coordinates, enter them in the order of latitude and longitude with a comma separator.')
     parser.add_argument('--list', '-li',
-                        help='Only needed in the case of discrete space. '
-                             'Specify the file name of your list of coordinates with filename extension. '
+                        help='Only compulsory for discrete space analysis. '
+                             'Use a location list with its filename extension as an input. '
                              'This file should be in the csv format with a comma (",") separator, '
                              'and comprised of three columns with a specific header of "location,latitude,longitude".')
     parser.add_argument('--format', '-f',  choices=['csv'],
-                        help='Optional: If you want to check the output in a table, use "csv" in this argument.')
+                        help='It is optional. If you want to check the output in a table, use "csv" in this argument.')
 
     args = parser.parse_args()
     tree = str(args.tree)
