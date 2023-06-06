a9861e53ce7acb5aea02347eaffc94af8615e136
YIMIN LI <yiminflorent@gmail.com>
2023-06-06 20:09:02 +0200

    Update regions.py

--- scripts/environmental_layer_generator/regions.py
+++ scripts/environmental_layer_generator/regions.py
@@ -6,22 +6,22 @@ import geopandas
 def main():
     welcome = "You can use this tool to create environmental layers using tabular data."
     parser = argparse.ArgumentParser(description=welcome)
+    parser.add_argument('--data', '-d', required=True,
+                        help='Specify the environmental tabular data you want to visualise (.csv, comma-delimited).')
+    parser.add_argument('--locationColumn', '-lc', required=True,
+                        help='In the CSV file, find the column that stores the location information.')
     parser.add_argument('--map', '-m', required=True,
                         help='Specify the input boundary map in GeoJSON format (.geojson).')
     parser.add_argument('--locationVariable', '-lv', required=True,
-                        help='In the GeoJSON input file, find the property that represents the location variable.')
-    parser.add_argument('--data', '-d', required=True,
-                        help='Specify the environmental data you want to visualise in an environmental layer (.csv, comma-delimited).')
-    parser.add_argument('--locationColumn', '-lc', required=True,
-                        help='In the CSV file, find the column that holds the location information.')
+                        help='In the GeoJSON input map, find a property that represents the location variable.')
     parser.add_argument('--output', '-o', required=True,
-                        help='Give a file name in which to store the output environmental layer (.geojson).')
+                        help='Give a name to the output environmental data layer (.geojson).')
 
     args = parser.parse_args()
-    map = str(args.map)
-    location_variable = str(args.locationVariable)
     data = str(args.data)
     location_column = str(args.locationColumn)
+    map = str(args.map)
+    location_variable = str(args.locationVariable)
     output = str(args.output)
     
     print("Started processing, please wait...")
