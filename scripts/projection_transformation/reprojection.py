f3f027d590e30a379e43da4c118925baae9eeb75
YIMIN LI <yiminflorent@gmail.com>
2023-08-21 16:23:41 +0200

    Update reprojection.py

--- scripts/projection_transformation/reprojection.py
+++ scripts/projection_transformation/reprojection.py
@@ -10,29 +10,29 @@ def main():
                         help='Specify the comma-delimited input file with filename extension (.csv).')
     parser.add_argument('--lat', '-la', required=True,
                         help='Type in the field names of source latitudes with a comma separator.')
-    parser.add_argument('--lng', '-ln', required=True,
+    parser.add_argument('--lon', '-lo', required=True,
                         help='Type in the field names of source longitudes with a comma separator.')
-    parser.add_argument('--src', '-s', required=True,
+    parser.add_argument('--source', '-s', required=True,
                         help='Type in EPSG code of source CRS, e.g. 27700.')
-    parser.add_argument('--trg', '-t', required=True,
+    parser.add_argument('--target', '-t', required=True,
                         help='Type in EPSG code of target CRS. e.g. 4326.')
     parser.add_argument('--output', '-o', required=True,
                         help='Create a name with filename extension (.csv) for the output file.')
 
     args = parser.parse_args()
     input_file = str(args.input)
-    src_lat = str(args.lat).split(',')
-    src_lng = str(args.lng).split(',')
-    src_crs = 'EPSG:'+str(args.src)
-    trg_crs = 'EPSG:'+str(args.trg)
+    lat_ori = str(args.lat).split(',')
+    lon_ori = str(args.lon).split(',')
+    source_crs = 'EPSG:'+str(args.source)
+    target_crs = 'EPSG:'+str(args.target)
     output_file = str(args.output)
 
     print("Started processing, please wait...")
     df = pd.read_csv(input_file, delimiter=",")
-    transformer = Transformer.from_crs(src_crs, trg_crs)
-    for i in range(len(src_lng)):
-        df[src_lat[i]+'_original'], df[src_lng[i]+'_original'] = df[src_lat[i]], df[src_lng[i]]
-        df[src_lat[i]], df[src_lng[i]] = transformer.transform(df[src_lng[i]].tolist(), df[src_lat[i]].tolist())
+    transformer = Transformer.from_crs(source_crs, target_crs)
+    for i in range(len(lon_ori)):
+        df[lat_ori[i]+'_original'], df[lon_ori[i]+'_original'] = df[lat_ori[i]], df[lon_ori[i]]
+        df[lat_ori[i]], df[lon_ori[i]] = transformer.transform(df[lon_ori[i]].tolist(), df[lat_ori[i]].tolist())
     df.to_csv(output_file, sep=",", index=False)
     print('The result has been successfully stored as "' + output_file + '" in the current directory!')
 
